d7d64bd1eb5ccb11a0214717c174a8514e3a7a23
Aroh Abuchi <arohabuchi@gmail.com>
2024-05-28 17:34:26 +0300

    Add project files.

---
+++ WealthZone/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WealthZone.Models;
+
+namespace WealthZone.Data
+{
+    public class AppDbContext:DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Stock> stocks { get; set; }
+        public DbSet<Comment> comments  { get; set; }
+    }
+}
